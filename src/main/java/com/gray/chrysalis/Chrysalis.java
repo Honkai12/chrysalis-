4cfa7e09d29b86fe978682d95d02d35a43c288b8
Honkai12 <mcnabb1949@gmail.com>
2025-12-01 19:36:08 -0500

    curios work push

--- src/main/java/com/gray/chrysalis/Chrysalis.java
+++ src/main/java/com/gray/chrysalis/Chrysalis.java
@@ -1,7 +1,6 @@
 package com.gray.chrysalis;
 
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -17,8 +16,8 @@ import net.minecraft.client.Minecraft;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod {
+@Mod(Chrysalis.MODID)
+public class Chrysalis {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "chrysalis";
     // Directly reference a slf4j logger
@@ -34,7 +33,7 @@ public class ExampleMod {
 
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = ExampleMod.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = Chrysalis.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     static class ClientModEvents {
         @SubscribeEvent
         static void onClientSetup(FMLClientSetupEvent event) {
