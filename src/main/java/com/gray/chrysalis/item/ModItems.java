9c21615d072a83bbc6ad661d23b25308ac11f25b
Honkai12 <mcnabb1949@gmail.com>
2025-12-06 15:07:52 -0500

    curios work push

--- src/main/java/com/gray/chrysalis/item/ModItems.java
+++ src/main/java/com/gray/chrysalis/item/ModItems.java
@@ -12,11 +12,8 @@ import java.util.function.Supplier;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Chrysalis.MODID);
 
-    public static final DeferredItem<Item> DEEP_BLUE_DORMANT_CURIO = ITEMS.register("deep_blue_dormant",
-            () -> new Item(new Item.Properties()));
-
     // dormant deep blue
-    public static final Supplier<CurioBaseItem> DEEP_BLUE_DORMANT_CURIO = ITEMS.register("deep_blue_dormant",DeepBlueDormantCurio::new);
+    public static final Supplier<CurioBaseItem> DEEP_BLUE_DORMANT_CURIO = ITEMS.register("deep_blue_dormant", DeepBlueDormantCurio::new);
 
     public static void register(IEventBus eventBus){
     ITEMS.register(eventBus);}
