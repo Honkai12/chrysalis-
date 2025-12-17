dd8e3b36b884d150e1744e05c8b702292e77e18b
Honkai12 <mcnabb1949@gmail.com>
2025-12-17 14:19:28 -0500

    curios work push

--- src/main/java/com/gray/chrysalis/ModEvents.java
+++ src/main/java/com/gray/chrysalis/ModEvents.java
@@ -1,20 +1,23 @@
 package com.gray.chrysalis;
 
+import com.gray.chrysalis.item.ModItems;
+import com.gray.chrysalis.item.curios.DeepBlueDormantCurio;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.NeoForge;
 import org.checkerframework.checker.units.qual.C;
 import top.theillusivec4.curios.api.event.DropRulesEvent;
+import top.theillusivec4.curios.api.type.capability.ICurio;
+
+import java.util.function.Predicate;
 
 @EventBusSubscriber(modid = Chrysalis.MODID,value = Dist.DEDICATED_SERVER)
 public class ModEvents {
-    public Chrysalis(IEventBus modBus){
-        NeoForge.EVENT_BUS.addListener(Chrysalis:DropRulesEvent);
-
+    @SubscribeEvent
+    private static void curioDropRules(DropRulesEvent event){
+        event.addOverride(itemStack -> {return itemStack.is(ModItems.DEEP_BLUE_DORMANT_CURIO.get());}, ICurio.DropRule.ALWAYS_KEEP
+        );
     }
-
-
-
-
 }
