ba34a95b5a5c8d25ffc02c55ce054722fe50cbbd
Honkai12 <mcnabb1949@gmail.com>
2026-01-12 13:57:51 -0500

    closer to have a beta build

--- src/main/java/com/gray/chrysalis/item/ModItems.java
+++ src/main/java/com/gray/chrysalis/item/ModItems.java
@@ -3,10 +3,9 @@ package com.gray.chrysalis.item;
 import com.gray.chrysalis.Chrysalis;
 import com.gray.chrysalis.item.curios.DeepBlueDormantCurio;
 import com.gray.chrysalis.item.curios.OvergrownJadeDormantCurio;
+import com.gray.chrysalis.item.curios.ThunderingDrakeDormantCurio;
 import io.redspace.ironsspellbooks.item.curios.CurioBaseItem;
-import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
@@ -16,7 +15,11 @@ public class ModItems {
 
     // dormant deep blue
     public static final Supplier<CurioBaseItem> DEEP_BLUE_DORMANT_CURIO = ITEMS.register("deep_blue_dormant", DeepBlueDormantCurio::new);
+   // dormant overgrown jade
     public static final Supplier<CurioBaseItem> OVERGROWN_JADE_DORMANT_CURIO = ITEMS.register("overgrown_jade_dormant", OvergrownJadeDormantCurio::new);
+   // dormant thundering drake
+   public static final Supplier<CurioBaseItem> THUNDERING_DRAKE_DORMANT_CURIO = ITEMS.register("thundering_drake_dormant", ThunderingDrakeDormantCurio::new);
+
     public static void register(IEventBus eventBus){
     ITEMS.register(eventBus);}
 }
